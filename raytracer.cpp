a09852b900060c3e8d9fe4536255c5aea196fa8b
Udyan Khurana <udyank@gmail.com>
2014-06-21 16:47:35 +0530

    added abstract shape class and inheritance

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<limits>
+#include<string.h>
 #include<stdio.h>
 #include<time.h>
 #include<math.h>
-#define INT_MIN -2147483647
 #define EPSILON 0.000001
+#define MIN(a,b) (((a)<(b))?(a):(b))
 
 class vector
 {	public:
@@ -37,63 +39,80 @@ class vector
 	}
 };
 
-struct tri
-{	vector a,b,c;	//for the 3 vertices
-	int color;	//value 0-255
-};
-
-struct sphere
-{	vector centre;
-	float radius;
-	int color;
-};
-
 class ray
 {	public: 
 	vector rayorig,raydir;	//origin and direction of ray
-	float t;	// as ray R = origin + t * direction
-	ray(vector x,vector y): rayorig(x), raydir(y), t(0.0) {}
-	float triangle_intersect(tri t)	//Moller-Trumbore algorithm
+	float tmin,tmax;	// as ray R = origin + t * direction
+	int id;	// to check if intersection happens or not (if yes, with which shape number it happens)
+	ray(vector x,vector y): rayorig(x), raydir(y), tmin(0.0), tmax(std::numeric_limits<float>::max()), id(-1) {}
+};
+
+class shape
+{	public:
+	int shapeid,color;	//can be made public also
+	virtual bool intersect(ray &r) = 0;	//virtual function to get intersection
+	shape(int col,int id): color(col), shapeid(id) {}
+};
+
+class triangle: public shape
+{	public:
+	vector a,b,c;	//for the 3 vertices
+	triangle(vector x,vector y,vector z,int col,int id): shape(col,id), a(x), b(y), c(z) {}
+	~triangle() {};
+	bool intersect(ray &r)	//Moller-Trumbore algorithm
 	{	vector v1,v2;	//calculate vectors for side edges
-		v1 = t.b - t.a;
-		v2 = t.c - t.a;
-		vector p = raydir.cross(v2);
+		v1 = b - a;
+		v2 = c - a;
+		vector p = (r.raydir).cross(v2);
 		float det = p.dot(v1);
 		if(det > -EPSILON && det < EPSILON) //means vector is parallel to the plane (int. at inifinity)
-			return 0;
+			return false;
 		float inv_det = 1.0/det;
-		vector T = rayorig - t.a;
+		vector T = r.rayorig - a;
 		float U = inv_det * T.dot(p);	//U,V: barycentric coordinates
 		if (U <0.0 || U > 1.0) //the intersection is outside the triangle
-			return 0;
+			return false;
 		vector q = T.cross(v1);
-		float V = raydir.dot(q) * inv_det;
+		float V = (r.raydir).dot(q) * inv_det;
 		if (V <0.0 || U+V > 1.0) 
-			return 0;
+			return false;
 		float param = v2.dot(q) * inv_det;
-		if(param > EPSILON)
-			return param;	//the 't' value returned if intersection found
-		return 0;
+		if(param > EPSILON && param < r.tmax)
+		{	r.tmax = param;	//the 't' value assigned if intersection found
+			r.id = shapeid;
+			return true;
+		}
+		return false;
 	}
-	float sphere_intersect(sphere s)
-	{	float a,b,c;
-		vector diff = rayorig - s.centre;
-		a=raydir.dot(raydir);
-		b=2*(raydir.dot(diff));
-		c=diff.dot(diff) - s.radius*s.radius;
+};
+
+struct sphere: public shape
+{	public:
+	vector centre;
+	float radius;
+	sphere(vector c,float r,int col,int id): shape(col,id), centre(c), radius(r) {}
+	~sphere() {};
+	bool intersect(ray &r)
+	{	float a,b,c,ans;
+		vector diff = r.rayorig - centre;
+		a=(r.raydir).dot(r.raydir);
+		b=2*((r.raydir).dot(diff));
+		c=diff.dot(diff) - radius*radius;
 		float disc = b*b - 4*a*c;
 		if (disc == 0)
-			return (-(b/(2*a)));
+			ans = (-(b/(2*a)));
 		else if(disc < 0)
-			return 0;
+			return false;
 		else
 		{	float r1,r2;
 			r1 = (-b + sqrt(disc))/(2*a);
 			r2 = (-b - sqrt(disc))/(2*a);
-			if(r1 < r2)
-				return r1;
-			else
-				return r2;
+			ans = MIN(r1,r2);
+		}
+		if(ans < r.tmax)
+		{	r.tmax = ans;
+			r.id = shapeid;
+			return true;
 		}
 	}
 };
@@ -101,12 +120,14 @@ class ray
 int main() 
 {	
 	int width, height;
-	int numtris,numspheres,i,j,k,color;
-	int bgcolor=10;
+	int numshapes,i=0,j=0,k=0,color,bgcolor=10;
 	// Allocate storage for output image and triangle array
-	unsigned int pixelgrid[500][500]={0};	//so width,height limit = 250
-	tri triarray[50];
-	sphere spherearray[50];
+	unsigned int pixelgrid[500][500]={0};	//width,height limit = 500
+	char type[25];
+	shape *shapes[50];
+	vector a = vector(0,0,0);
+	triangle t[10] = triangle(a,a,a,0,0);
+	sphere s[10] = sphere(a,0,0,0);
 
 	struct timespec t1, t2;
 	clock_gettime(CLOCK_MONOTONIC, &t1);
@@ -114,23 +135,38 @@ int main()
 
 	//Read Input
 	scanf("%d %d",&height,&width);	//height=rows, width=cols
-	scanf("%d",&numtris);
-	for(i=0;i<numtris;i++)
-	{	scanf("%d",&triarray[i].color);
-		scanf("%f %f %f",&xx,&yy,&zz);	//pt 1
-		triarray[i].a = vector(xx,yy,zz);
-		scanf("%f %f %f",&xx,&yy,&zz); //pt 2
-		triarray[i].b = vector(xx,yy,zz);
-		scanf("%f %f %f",&xx,&yy,&zz); //pt 3
-		triarray[i].c = vector(xx,yy,zz);
+	scanf("%d",&numshapes);
+	for(i=0;i<numshapes;i++)
+	{	scanf("%s",type);
+		if(!strcmp(type,"Triangle"))
+		{	int col;
+			vector x,y,z;
+			scanf("%d",&col);
+			scanf("%f %f %f",&xx,&yy,&zz);	//pt 1
+			x = vector(xx,yy,zz);
+			scanf("%f %f %f",&xx,&yy,&zz); //pt 2
+			y = vector(xx,yy,zz);
+			scanf("%f %f %f",&xx,&yy,&zz); //pt 3
+			z = vector(xx,yy,zz);
+			t[j] = triangle(x,y,z,col,i);
+			shapes[i] = &t[j++]; 
+			/* What we did earlier: 
+			tiangle t = triangle(x,y,z,col,i);
+			shapes[i] = &t; 
+			*/   
+		}
+		else if(!strcmp(type,"Sphere"))
+		{	int col; vector c; float r;
+			scanf("%d",&col);
+			scanf("%f %f %f",&xx,&yy,&zz);	//centre
+			c = vector(xx,yy,zz);
+			scanf("%f",&r); //radius
+			s[k] = sphere(c,r,col,i);
+			shapes[i] = &s[k++]; 
+		}
 	}
-	scanf("%d",&numspheres);
-	for(i=0;i<numspheres;i++)
-	{	scanf("%d",&spherearray[i].color);
-		scanf("%f %f %f",&xx,&yy,&zz);	//centre
-		spherearray[i].centre = vector(xx,yy,zz);
-		scanf("%f",&spherearray[i].radius); //radius
-	}
+	for(k=0;k<numshapes;k++)
+		printf("id of shape %d is %d\n",k,shapes[k]->shapeid);
 	vector orig = vector(height/2,width/2,25);	//Fixed Origin: Simulating a Pin-hole camera
 	for(i=0;i<height;i++)
 	{	for(j=0;j<width;j++)
@@ -138,26 +174,12 @@ int main()
 			dir = dir - orig; //direction vector
 			dir = dir.normalize();
 			ray r = ray(orig,dir);
-			for(k=0;k<numtris;k++)
-			{	float x = r.triangle_intersect(triarray[k]);	//this is the 't' parameter
-				if(x!=0)
-				{	if(r.t!=0.0 && x < r.t || r.t==0.0) //either no pvs. intersection or closer intersection found 
-					{	pixelgrid[i][j] = triarray[k].color;
-						r.t = x;
-					}
-				}
-			}
-			for(k=0;k<numspheres;k++)
-			{	float x = r.sphere_intersect(spherearray[k]);	//this is the 't' parameter
-				if(x!=0)
-				{	if(r.t!=0.0 && x < r.t || r.t==0.0) //either no pvs. intersection or closer intersection found 
-					{	pixelgrid[i][j] = spherearray[k].color;
-						r.t = x;
-					}
-				}
-			}
-			if(pixelgrid[i][j]==0)
-				pixelgrid[i][j] = bgcolor;
+			int col = bgcolor;
+			for(k=0;k<numshapes;k++)
+				shapes[k]->intersect(r);
+			if(r.id != -1)
+				col=shapes[r.id]->color;	//should this be '->' or '.' ??
+			pixelgrid[i][j] = col;
 		}
 	}
 
